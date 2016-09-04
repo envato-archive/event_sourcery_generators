49b466bf77333c75ce1c62cc1864cd26f18855ec
Sebastian von Conrad <sebastian@vonconrad.com>
2016-09-04 14:33:36 +1000

    Expand/redo command generation
    I didn't like how the command generator was so verbose in its
    arguments, so I decided to reduce the number of arguments and
    make some assumptions. I'm also expanding out the generation
    of commands to inject methods into the aggregate.rb file if one
    already exists. Finally, we're also adding the command class
    itself.

--- lib/event_sourcery_generators/generators/command.rb
+++ lib/event_sourcery_generators/generators/command.rb
@@ -5,22 +5,72 @@ module EventSourceryGenerators
 
       argument :aggregate
       argument :command
-      argument :event
 
       def self.source_root
         File.join(File.dirname(__FILE__), 'templates/command')
-      end
-
-      def create_aggregate_file
-        template('aggregate.rb.tt', "domain/#{aggregate}/aggregate.rb")
       end
 
       def create_respository_file
         template('repository.rb.tt', "domain/#{aggregate}/repository.rb")
       end
 
-      def create_command_handler_file
+      def create_or_inject_into_aggregate_file
+        aggregate_file = "domain/#{aggregate}/aggregate.rb"
+
+        @command_method = erb_file('aggregate/command_method.rb.tt')
+        @apply_event_method = erb_file('aggregate/apply_event_method.rb.tt')
+
+        if File.exist?(aggregate_file)
+          insert_into_file(aggregate_file, @command_method, after: "include EventSourcery::AggregateRoot\n")
+          insert_into_file(aggregate_file, @apply_event_method, after: "private\n")
+        else
+          template('aggregate.rb.tt', aggregate_file)
+        end
+      end
+
+      def create_command_files
+        template('command.rb.tt', "domain/#{aggregate}/#{command}/command.rb")
         template('command_handler.rb.tt', "domain/#{aggregate}/#{command}/command_handler.rb")
+      end
+
+      def inject_command_to_api
+        insert_into_file('app/command/api.rb', after: "def self.registered(app)\n") do
+          erb_file('api_endpoint.rb.tt')
+        end
+      end
+
+      private
+
+      def aggregate_name
+        aggregate.underscore
+      end
+
+      def aggregate_class_name
+        aggregate.camelize
+      end
+
+      def command_name
+        command.underscore
+      end
+
+      def command_class_name
+        command_name.camelize
+      end
+
+      def event_name
+        # Primitive way to get past tense of the command,
+        # will likely need more work in the future
+        past_tense = command.gsub(/e$/, '') + 'ed'
+        [ aggregate, past_tense ].map(&:underscore).join('_')
+      end
+
+      def erb_file(file)
+        path = File.join(self.class.source_root, file)
+        ERB.new(::File.binread(path), nil, "-", "@output_buffer").result(binding)
+      end
+
+      def project_name
+        @project_name ||= File.split(Dir.pwd).last
       end
     end
   end
