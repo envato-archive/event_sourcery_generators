37ba2894ef8fbc563e1438fd4b5d96035034c793
Chun-wei Kuo <Dendoh@gmail.com>
2017-06-14 17:39:38 +1000

    Generate command with the latest structure of event sourcery apps

--- lib/event_sourcery_generators/cli.rb
+++ lib/event_sourcery_generators/cli.rb
@@ -12,7 +12,7 @@ module EventSourceryGenerators
     register(Generators::Project, 'new', 'new [PROJECT NAME]', 'Creates a new EventSourcery project')
 
     # Creating components inside a project
-    register(Generators::Command, 'generate:command', 'generate:command [AGGREGATE] [COMMAND] [EVENT]', 'Generates a new COMMAND for AGGREGATE to create EVENT')
+    register(Generators::Command, 'generate:command', 'generate:command [AGGREGATE] [COMMAND]', 'Generates a new COMMAND for AGGREGATE')
     register(Generators::Query, 'generate:query', 'generate:query [NAME]', 'Generates a new query with the name NAME')
     register(Generators::Reactor, 'generate:reactor', 'generate:reactor NAME [EVENT1 EVENT2 ...]', 'Generates a new Reactor with the name NAME and a list of EVENTs optionally')
   end
--- lib/event_sourcery_generators/generators/command.rb
+++ lib/event_sourcery_generators/generators/command.rb
@@ -10,31 +10,26 @@ module EventSourceryGenerators
         File.join(File.dirname(__FILE__), 'templates/command')
       end
 
-      def create_respository_file
-        template('repository.rb.tt', "commands/#{aggregate}/repository.rb")
-      end
-
       def create_or_inject_into_aggregate_file
-        aggregate_file = "commands/#{aggregate}/#{aggregate}.rb"
+        aggregate_file = "app/aggregates/#{aggregate.underscore}.rb"
 
-        @command_method = erb_file('aggregate/command_method.rb.tt')
-        @apply_event_method = erb_file('aggregate/apply_event_method.rb.tt')
+        @command_method     = erb_file('aggregate/command_method.rb.tt').chomp
+        @apply_event_method = erb_file('aggregate/apply_event_method.rb.tt').chomp
 
         if File.exist?(aggregate_file)
           insert_into_file(aggregate_file, @command_method, after: "include EventSourcery::AggregateRoot\n")
-          insert_into_file(aggregate_file, @apply_event_method, after: "private\n")
+          insert_into_file(aggregate_file, @apply_event_method, after: "include EventSourcery::AggregateRoot\n")
         else
           template('aggregate.rb.tt', aggregate_file)
         end
       end
 
-      def create_command_files
-        template('command.rb.tt', "commands/#{aggregate}/#{command}/command.rb")
-        template('command_handler.rb.tt', "commands/#{aggregate}/#{command}/command_handler.rb")
+      def create_command_file
+        template('command.rb.tt', "app/commands/#{aggregate}/#{command}.rb")
       end
 
       def inject_command_to_api
-        insert_into_file('web/commands/api.rb', after: "def self.registered(app)\n") do
+        insert_into_file('app/web/server.rb', after: "< Sinatra::Base\n") do
           erb_file('api_endpoint.rb.tt')
         end
       end
@@ -64,6 +59,10 @@ module EventSourceryGenerators
         [ aggregate, past_tense ].map(&:underscore).join('_')
       end
 
+      def event_class_name
+        event_name.underscore.camelize
+      end
+
       def erb_file(file)
         path = File.join(self.class.source_root, file)
         ERB.new(::File.binread(path), nil, "-", "@output_buffer").result(binding)
@@ -71,6 +70,10 @@ module EventSourceryGenerators
 
       def project_name
         @project_name ||= File.split(Dir.pwd).last
+      end
+
+      def project_class_name
+        project_name.underscore.camelize
       end
     end
   end
